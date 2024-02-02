7d8f5b64a4437f1ba7250cebb56136535730f17b
Noe Osorio <osorionoe@gmail.com>
2024-02-02 12:45:55 -0600

    Add router

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
-import "react-native-url-polyfill/auto"
-import { StyleSheet, Text, View } from 'react-native';
-import ChatScreen from './src/features/chat/screens/ChatScreen';
+import "react-native-url-polyfill/auto";
+import { StyleSheet, Text, View } from "react-native";
+import ChatScreen from "./src/features/chat/screens/ChatScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <ChatScreen />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Chat">
+        <Stack.Screen name="Chat" component={ChatScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
